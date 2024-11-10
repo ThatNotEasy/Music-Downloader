df913be7bccd3b1929b7bac2cffaeb27d78a2a71
Pari Malam <apid9755@gmail.com>
2024-11-10 08:27:38 +0800

    add logging

--- modules/logging.py
+++ modules/logging.py
@@ -1,8 +1,17 @@
-import logging, coloredlogs
+import logging, coloredlogs, os
 
 def setup_logging():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    # Set up the logger
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    coloredlogs.install(level='DEBUG', fmt=log_format, logger=logger)
+    coloredlogs.install(level='INFO', fmt=log_format, logger=logger)
+    file_handler = logging.FileHandler('logs/logs.log')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(file_handler)
+    
     return logger
