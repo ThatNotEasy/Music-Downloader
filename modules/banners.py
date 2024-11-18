ac057adb039ebbbb84a1a25ca4bdfea2e2777d0c
Pari Malam <you@example.com>
2024-11-18 13:06:58 +0800

    update

--- modules/banners.py
+++ modules/banners.py
@@ -15,4 +15,4 @@ def banners():
     stdout.write(""+Fore.YELLOW   +"╔════════════════════════════════════════════════════════════════════════════╝\n")
     stdout.write(""+Fore.YELLOW   +"║ \x1b[38;2;255;20;147m• "+Fore.GREEN+"GITHUB             "+Fore.RED+"    |"+Fore.LIGHTWHITE_EX+"   GITHUB.COM/THATNOTEASY                        "+Fore.YELLOW+"║\n")
     stdout.write(""+Fore.YELLOW   +"╚════════════════════════════════════════════════════════════════════════════╝\n") 
-    print(f"{Fore.YELLOW}[Music-Downloader] - {Fore.GREEN}Downloading Music tracks and albums as MP3 files \n{Fore.RESET}")
+    print(f"{Fore.YELLOW}[Music-Downloader] - {Fore.GREEN}Downloading Music tracks and albums as MP3 files - {Fore.RED}[V1.4] \n{Fore.RESET}")
